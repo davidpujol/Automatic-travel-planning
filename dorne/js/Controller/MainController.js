7d62668e85b5e0c740a0e5d8e5ebae1383ab33ee
oriolaranda <oriolaranda@gmail.com>
2018-10-20 00:34:15 +0200

    Oriol

--- dorne/js/Controller/MainController.js
+++ dorne/js/Controller/MainController.js
@@ -1,20 +1,248 @@
 app.controller('controllerProducts', ['$scope', function($scope) {
     scope.cities = [
         {
-            name:   ,
-            puntuacio:  ,
+            name: "Modena"  ,
+            puntuacio:,
             llocs: [
                 {
-                    namePlace:,
-                    type:
+                    namePlace: "Osteria Francescana",
+                    type: 'R'
                 },
-
+                {
+                    namePlace: "L'Erba de Re",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Strada Facendo",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Torre della Ghirlandina",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Modena Cathedral",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Museo Casa Enzo Ferrari",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Galleria Estense",
+                    type: 'M'
+                },
             ]
 
 
-        }
+        },
+        {
+            name: "Napols",
+            puntuacio:,
+            llocs: [
+                {
+                    namePlace:"National Archeologic Museum",
+                    type: 'M'
+                },
+                {
+                    namePlace:"Bourbon Tunnel",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Santi Filippo e Giacomo",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Museo Galileo Napols",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Mount Vesuvius",
+                    type: 'N'
+                },
+                {
+                    namePlace: "Royal Palace of Naples",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Castell dell' Ovo",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Piazza del Plebiscito",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Capella Sansevero",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Naples Cathedral",
+                    type: 'M'
+                },
+                {
+                    namePlace: "San Francesco di Paola",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Pompei",
+                    type: 'N'
+                },
+                {
+                    namePlace: "II Comandante",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Palazzo Petrucci",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Vertias",
+                    type: 'R'
+                },
 
-
+            ]
+        },
+        {
+            name: "Milan",
+            puntuacio:,
+            llocs: [
+                {
+                    namePlace:"Sforza Castle",
+                    type: 'M'
+                },
+                {
+                    namePlace:"Pinacoteca di Brera",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Galleria Vittorio Emanuele II",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Teatro alla Scala",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Santa Maria delle Grazie",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Pinacoteca di Brera",
+                    type: 'M'
+                },
+                {
+                    namePlace: "San Siro Stadium",
+                    type: 'F'
+                },
+                {
+                    namePlace: "Porta Sempione",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Piazza Mercanti",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Enrico Bartolini al Mudec",
+                    type: 'R'
+                },
+                {
+                    namePlace: "II Lueogo di Aimo e Nadia",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Seta by Antonio Guida",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Essenza",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Vun",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Safaripark",
+                    type: 'F'
+                },
+                {
+                    namePlace: "Constraste",
+                    type: 'R'
+                },
+            ]
+        },
+        {
+            name: "Rome",
+            puntuacio:,
+            llocs: [
+                {
+                    namePlace:"Galleria Borghese",
+                    type: 'M'
+                },
+                {
+                    namePlace:"Castel Sant'Angelo",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Capitoline Museums",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Palazzo Altemps",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Welcome to Rome",
+                    type: 'M'
+                },
+                {
+                    namePlace: "National Roman Museum",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Parque dei Mostri",
+                    type: 'F'
+                },
+                {
+                    namePlace: "Galleria Borghese",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Castel Sant'Angelo",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Capitoline Museums",
+                    type: 'M'
+                },
+                {
+                    namePlace: "Palazzo Altemps",
+                    type: 'M'
+                },
+                {
+                    namePlace: "National Roman Museum",
+                    type: 'M'
+                },
+                {
+                    namePlace: "La Pergola",
+                    type: 'R'
+                },
+                {
+                    namePlace: "La Terrazza",
+                    type: 'R'
+                },
+                {
+                    namePlace: "Tordomatto",
+                    type: 'R'
+                },
+                {
+                    namePlace: "II Pagliaccio",
+                    type: 'R'
+                },
+            ]
+        },
     ]
 
 
