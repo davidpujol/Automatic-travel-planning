943db9b07d420296382b6f2f4cfbad7b4dc4f543
oriolaranda <oriolaranda@gmail.com>
2018-10-20 13:43:29 +0200

    Oriol

--- dorne/js/directives/findRoute.js
+++ dorne/js/directives/findRoute.js
@@ -22,16 +22,79 @@ app.directive ('findRoute', function (){
 
 
             //Inicialize map
-            var adjacencyList = scope.cities.reduce (function (map, city) {
-                map[city.name] = calculateAdjacent (city);
+            var adjecencyList = scope.cities.reduce (function (map, city) {
+                map[city.name] = calculateAdjencents (city);
                 return map;
             }, {});
+
+
+
+            algorithm = function(graph, startVertex) {
+            const distances = {};
+            const visitedVertices = {};
+            const previousVertices = {};
+            const queue = new PriorityQueue();
+
+            // Init all distances with infinity assuming that currently we can't reach
+            // any of the vertices except start one.
+
+            for (var key of graph.keys()){
+                distances[key] = Infinity;
+                previousVertices[key] = null;
+            }
+
+            distances[startVertex] = 0;
+
+            // Init vertices queue.
+            queue.add(startVertex, distances[startVertex]);
+
+            while (!queue.isEmpty()) {
+                const currentVertex = queue.poll();
+                for (var neighbor of graph[currentVertex].second()) {
+                    // Don't visit already visited vertices.
+                    if (!visitedVertices[neighbor]) {
+                        // Update distances to every neighbor from current vertex
+                        let weight = graph[neighbor].first();
+                        const existingDistanceToNeighbor = distances[neighbor];
+                        const distanceToNeighborFromCurrent = distances[currentVertex] + weight;
+
+                        if (distanceToNeighborFromCurrent < existingDistanceToNeighbor) {
+                            distances[neighbor] = distanceToNeighborFromCurrent;
+
+                            // Change priority.
+                            if (queue.hasValue(neighbor)) {
+                                queue.changePriority(neighbor, distances[neighbor]);
+                            }
+
+                            // Remember previous vertex.
+                            previousVertices[neighbor] = currentVertex;
+                        }
+
+                        // Add neighbor to the queue for further visiting.
+                        if (!queue.hasValue(neighbor)) {
+                            queue.add(neighbor, distances[neighbor]);
+                        }
+                    }
+                }
+
+                // Add current vertex to visited ones.
+                visitedVertices[currentVertex] = currentVertex;
+            }
+
+            return {
+                distances,
+                previousVertices,
+            };
+        }
+
+
 
 
 
 
 
         }
+
 
     };
 
